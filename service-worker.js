67410c30e33b03bc5a82d78eb750e5c7fbcdc953
dentcast-hub <foad.shahabian@gmail.com>
2025-11-05 00:08:15 +0330

    Update service-worker.js second

--- service-worker.js
+++ service-worker.js
@@ -1,41 +1,23 @@
-const CACHE_NAME = 'dentcast-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/noteCast/',
-  '/favicon-32.png',
-  '/favicon-192.png',
-  '/favicon-512.png'
-];
-
-// نصب و کش اولیه
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener('install', (event) => {
+  console.log('DentCast Service Worker installed.');
+  self.skipWaiting();
 });
 
-// واکنش به درخواست‌ها (فچ از کش یا اینترنت)
-self.addEventListener('fetch', event => {
+self.addEventListener('activate', (event) => {
+  console.log('DentCast Service Worker activated.');
+});
+
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
-// حذف کش‌های قدیمی هنگام آپدیت
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames =>
-      Promise.all(
-        cacheNames.map(cacheName => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            return caches.delete(cacheName);
+    caches.open('dentcast-cache').then((cache) => {
+      return cache.match(event.request).then((response) => {
+        return response || fetch(event.request).then((networkResponse) => {
+          if (event.request.url.startsWith('http')) {
+            cache.put(event.request, networkResponse.clone());
           }
-        })
-      )
-    )
+          return networkResponse;
+        });
+      });
+    })
   );
 });
